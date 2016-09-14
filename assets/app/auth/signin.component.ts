9ebd65394119f94724f49068be1d8c82b8ee707d
hvpulok <hvpulok@gmail.com>
2016-09-14 14:15:28 -0600

    Feat: Add user signin route to server and update related  auth service, component methods

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -3,6 +3,10 @@ import {OnInit} from "angular2/src/core/linker/interfaces";
 import {ControlGroup, Control} from "angular2/src/common/forms/model";
 import {FormBuilder} from "angular2/src/common/forms/form_builder";
 import {Validators} from "angular2/src/common/forms/validators";
+import {User} from "./user";
+import {AuthService} from "./auth.service";
+import {Router} from "angular2/router";
+
 @Component({
     selector: 'my-signin',
     template: `
@@ -35,10 +39,30 @@ import {Validators} from "angular2/src/common/forms/validators";
 
 export class SigninComponent implements OnInit{
     myForm: ControlGroup;
-    constructor(private _fb:FormBuilder){}
+    constructor(
+        private _fb:FormBuilder,
+        private _authService : AuthService,
+        private _router: Router
+
+    ){}
 
     onSubmit(){
-        console.log(this.myForm.value);
+        // console.log(this.myForm.value);
+
+        const user = new User(
+            this.myForm.value.email,
+            this.myForm.value.password
+        );
+
+        this._authService.signin(user)
+            .subscribe(
+                data => {
+                    localStorage.setItem('token', data.token);
+                    localStorage.setItem('userId', data.userId);
+                    this._router.navigateByUrl('/');
+                },
+                error => console.error(error)
+            )
     }
 
     ngOnInit(){
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express             = require('express');
 var router              = express.Router();
 var passwordHash        = require('password-hash');
+var jwt                 = require('jsonwebtoken');
 
 var User                = require('../models/user');
 
@@ -27,6 +28,38 @@ router.post('/', function (req, res, next) {
             obj: result
         });
     })
+});
+
+// definition of user signin route
+router.post('/signin', function (req, res, next) {
+    User.findOne({email : req.body.email}, function(err, doc){
+        if(err){
+            return res.status(404).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if(!doc){
+            return res.status(404).json({
+                title: 'No user found',
+                error: {message: 'User could not be found'}
+            });
+        }
+
+        if(!passwordHash.verify(req.body.password, doc.password)){
+            return res.status(404).json({
+                title: 'Could not sign you in',
+                error: {message : 'Invalid password'}
+            });
+        }
+
+        var token = jwt.sign({user: doc}, '@secret!', {expiresIn: 7200});
+        res.status(200).json({
+            message: 'Success',
+            token: token,
+            userId: doc._id
+        });
+    });
 });
 
 module.exports = router;
